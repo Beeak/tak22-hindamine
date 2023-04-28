1915f6e7aae7232a45262b6562158cb2288e4e67
beeak <dudetitan@gmail.com>
2023-04-28 09:04:20 +0300

    Completed

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const heading = document.getElementsByTagName('h1')[0];
 const catsContainer = document.getElementById('container');
 
-heading.innerText = 'Gen ja kassid';
+heading.innerText = 'Henry Kalvi ja kassid';
 
 fetch('https://catfact.ninja/breeds?limit=100')
-.then(res => res.json())
-.then(cats => {
+  .then(res => res.json())
+  .then(cats => {
 
-    cats.data.forEach( cat => {
+    cats.data.forEach(cat => {
 
-        const catDiv = document.createElement('div');
+      const catDiv = document.createElement('div');
+      catDiv.classList.add('cat');
+      catDiv.innerHTML = `
+        <div><strong>${cat.breed}</strong></div>
+        <div>${cat.country}</div>
+      `;
 
-        catDiv.classList.add('cat');
+      catDiv.style.border = 'none'; // remove border
 
-        catDiv.innerHTML = `
-            <div>${cat.breed}</div>
-            <div>${cat.country}</div>
-        `;
+      catsContainer.append(catDiv);
+      catDiv.style.margin = '5px';
 
-        catsContainer.append(catDiv);
-       
     });
-});
+  });
